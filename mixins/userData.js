d1ea82e3c4d08c4c0e9a7e3e6a0c62be50caf4d3
Zak Kolar <zakkolar@users.noreply.github.com>
2022-03-22 11:48:27 -0400

    Abstract userData mixin computed fields

--- mixins/userData.js
+++ mixins/userData.js
@@ -1,28 +1,18 @@
-export default {
-  computed: {
-    number: {
-      get () {
-        return this.$store.state.userData.number
-      },
-      set (value) {
-        this.$store.dispatch('userData/number', value)
-      }
+function makeComputed(scope, key){
+  return {
+    get() {
+      return this.$store.state[scope][key]
     },
-    color: {
-      get() {
-        return this.$store.state.userData.color
-      },
-      set(value) {
-        this.$store.dispatch('userData/color', value)
-      }
-    },
-    entertainment: {
-      get() {
-        return this.$store.state.userData.entertainment
-      },
-      set(value) {
-        this.$store.dispatch('userData/entertainment', value)
-      }
+    set(value) {
+      this.$store.dispatch(`${scope}/${key}`, value)
     }
   }
+}
+
+export default {
+  computed: {
+    number: makeComputed('userData', 'number'),
+    color: makeComputed('userData', 'color'),
+    entertainment: makeComputed('userData', 'entertainment'),
+  },
 }
