d9beca2cafa9c852a0a84c8e12e045b8184869c4
Zak Kolar <zakkolar@users.noreply.github.com>
2022-03-22 11:31:43 -0400

    Abstract store methods

--- store/userData.js
+++ store/userData.js
@@ -6,30 +6,31 @@ export const state = () => ({
   number: ds.retrieve('number'),
 })
 
-export const mutations = {
-  color(state, color){
-    state.color = color
-  },
-  entertainment(state, entertainment){
-    state.entertainment = entertainment
-  },
-  number(state, number){
-    state.number = number?.replace(/69/g, "68").replace(/420/,"429") || null
+function makeMutation(key, transform = v => v){
+  return (state, value) => {
+    state[key] = transform(value);
+    transform(value);
   }
 }
 
+export const mutations = {
+  color: makeMutation('color'),
+  entertainment: makeMutation('entertainment'),
+  number: makeMutation('number', number => number?.replace(/69/g, "68").replace(/420/,"429")),
+  firstName: makeMutation('firstName'),
+  lastName: makeMutation('lastName')
+}
+
+function makeAction (key) {
+  return ((context, value) => {
+    context.commit(key, value);
+    ds.store(key, context.state[key]);
+  })
+}
+
 export const actions = {
-  color({commit}, color) {
-    commit('color', color)
-    ds.store('color', color)
-  },
-  entertainment({commit}, entertainment){
-    commit('entertainment', entertainment)
-    ds.store('entertainment', entertainment)
-  },
-  number({commit}, number) {
-    commit('number', number)
-    ds.store('number', number)
-  }
+  color: makeAction('color'),
+  entertainment: makeAction('entertainment'),
+  number: makeAction('number'),
 }
 
