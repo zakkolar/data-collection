6d56749bc9f11b46168eeb272a2d9b096aa92ca6
Zak Kolar <zakkolar@users.noreply.github.com>
2022-05-11 09:11:06 -0400

    Refactor style loading

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="layout-default">
     <Nuxt />
     <div class="privacy">
       <RouterLink to="/privacy">Privacy</RouterLink>
@@ -8,18 +8,30 @@
 </template>
 <script>
 export default {
-}
-</script>
-<style lang="scss">
-@import '~/assets/style.css';
-@import '~/assets/bootstrap-reboot.min.css';
-
-.privacy {
-  text-align: center;
-  margin-bottom: 30px;
-  a {
-    color: white;
+  head: {
+    link: [
+      {
+        rel: 'stylesheet',
+        href: '/css/style.css'
+      },
+      {
+        rel: 'stylesheet',
+        href: '/css/bootstrap-reboot.min.css'
+      }
+    ]
   }
 }
+</script>
+<style lang="scss" scoped>
+.layout-default {
+    .privacy {
+      text-align: center;
+      margin-bottom: 30px;
 
+      a {
+        color: white;
+      }
+
+  }
+}
 </style>
