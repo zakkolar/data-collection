e0c9b2744373da17c56f9917eec9b51d283016e9
Zak Kolar <zakkolar@users.noreply.github.com>
2022-05-12 15:51:39 -0400

    fix positioning

--- pages/articles/_id.vue
+++ pages/articles/_id.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <nuxt-link to="/articles" class="btn btn-dark">Back to articles</nuxt-link>
+  <div class="col-md-8 offset-md-2">
+    <nuxt-link to="/articles" class="btn btn-dark mb-2">Back to articles</nuxt-link>
     <h1>{{ article.title }}</h1>
-    <nuxt-content :document="article"></nuxt-content>
+    <nuxt-content :document="article" ></nuxt-content>
     <hr style="border-color: rgba(0,0,0,0.4); margin-top:20px; margin-bottom:20px; border-width:1px">
     <p>{{article.source}}</p>
   </div>
